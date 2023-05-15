1a8b244a859e38ecd34f5d13f08b8c0668a7fe8b
SIUUVENOM <abdullahkamel750@gmail.com>
2023-05-15 14:40:53 +0300

    second commit

--- script.js
+++ script.js
@@ -17,54 +17,35 @@ const playerOne = document.querySelector('.player--0');
 
 const playerTwo = document.querySelector('.player--1');
 
-let scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+let scores, currentScore, activePlayer, playing;
+
+// switching player
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  playerOne.classList.toggle('player--active');
+  playerTwo.classList.toggle('player--active');
+};
+
 // starting condtions
-score1.textContent = 0;
+const resetGame = function () {
+  scores = [0, 0];
 
-score2.textContent = 0;
+  currentScore = 0;
 
-imageDice.classList.add('hidden');
+  activePlayer = 0;
 
-// rolling dice
-rollBtn.addEventListener('click', () => {
-  // generating random number
-  const randomRoll = Math.trunc(Math.random() * 6) + 1;
-  // display dice
-  imageDice.classList.remove('hidden');
-  imageDice.src = `dice-${randomRoll}.png`;
-  // check if dice number is not quall to 1
-  if (randomRoll !== 1) {
-    currentScore += randomRoll;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    playerOne.classList.toggle('player--active');
-    playerTwo.classList.toggle('player--active');
-  }
-});
+  playing = true;
 
-// handling holding score feautre
-holdBtn.addEventListener('click', () => {
-  // check if current score is higher than 0
-  if (currentScore > 0) {
-    scores[activePlayer] += currentScore;
-    document.getElementById(`score--${activePlayer}`).textContent =
-      scores[activePlayer];
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    playerOne.classList.toggle('player--active');
-    playerTwo.classList.toggle('player--active');
-  }
-});
+  // starting condtions
+  score1.textContent = 0;
 
-// reseting game
-resetBtn.addEventListener('click', () => {
+  score2.textContent = 0;
+
+  imageDice.classList.add('hidden');
+
+  playing = true;
   activePlayer = 0;
   scores = [0, 0];
   currentScore = 0;
@@ -73,6 +54,58 @@ resetBtn.addEventListener('click', () => {
   playerTwo.classList.remove('player--active');
   document.getElementById('current--0').textContent = 0;
   document.getElementById('current--1').textContent = 0;
-  document.getElementById('score--0').textContent = 0;
-  document.getElementById('score--1').textContent = 0;
+  score1.textContent = 0;
+  score2.textContent = 0;
+  playerOne.classList.remove('player--winner');
+  playerTwo.classList.remove('player--winner');
+};
+
+resetGame();
+
+// rolling dice
+rollBtn.addEventListener('click', () => {
+  if (playing) {
+    // generating random number
+    const randomRoll = Math.trunc(Math.random() * 6) + 1;
+    // display dice
+    imageDice.classList.remove('hidden');
+    imageDice.src = `dice-${randomRoll}.png`;
+    // check if dice number is not quall to 1
+    if (randomRoll !== 1) {
+      currentScore += randomRoll;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
 });
+
+// handling holding score feautre
+holdBtn.addEventListener('click', () => {
+  if (playing) {
+    // check if current score is higher than 0
+    if (currentScore > 0) {
+      scores[activePlayer] += currentScore;
+      document.getElementById(`score--${activePlayer}`).textContent =
+        scores[activePlayer];
+    }
+    // display wining player
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      imageDice.classList.add('hidden');
+    } else {
+      // switching player
+      switchPlayer();
+    }
+  }
+});
+
+// reseting game
+resetBtn.addEventListener('click', resetGame);
